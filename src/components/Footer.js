825d34b85d2d8453c04fd1b9e991a4b92e6098e7
tomislav98 <tomislavdz98@gmail.com>
2025-08-17 18:54:17 +0200

    Make footer layout responsive with flexbox

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,68 +22,70 @@ const handleSubscribeUs = async (e) => {
 const Footer = () => {
   return (
     <footer>
-      <div className="footer-content-1">
-        <p>
-          Your friendly guide through the world of code and tech. Join us as we
-          explore new technologies, share coding tips, and simplify complex
-          concepts—helping you grow your skills and stay ahead in the digital
-          world.
-        </p>
-        <p>© 2025 Your Website. All rights reserved.</p>
-      </div>
-      <div className="footer-content-2">
-        <div className="flex-row-content-2">
-          <div className="flex-col">
-            <ul>
-              <li>Home</li>
-              <li>About Us</li>
-              <li>Gallery</li>
-              <li>Contact Us</li>
-            </ul>
-          </div>
-          <div className="flex-col">
-            <ul>
-              <li>Contents</li>
-              <li>Stories</li>
-              <li>Guides</li>
-            </ul>
-          </div>
-        </div>
-        <div className="flex-row">
-          <button className="btn btn-secondary">
-            SUPPORT US <ArrowRight />
-          </button>
+      <div className="container-footer">
+        <div className="footer-content-1">
           <p>
-            fund independent journal
-            <br /> with 10$ per month
+            Your friendly guide through the world of code and tech. Join us as
+            we explore new technologies, share coding tips, and simplify complex
+            concepts—helping you grow your skills and stay ahead in the digital
+            world.
           </p>
+          <p>© 2025 Your Website. All rights reserved.</p>
         </div>
-      </div>
-      <div className="footer-content-3">
-        <h3>
-          <Mail />
-          Subscribe Us
-        </h3>
-        <p>
-          Stay in the loop! Subscribe to get the latest updates, news, and tips
-          delivered straight to your inbox.
-        </p>
-        <form onSubmit={handleSubscribeUs}>
-          <div className="flex-row">
-            <div className="input-container">
-              <input
-                name="user_name"
-                type="text"
-                placeholder="Your Email Address"
-              />
+        <div className="footer-content-2">
+          <div className="flex-row-content-2">
+            <div className="flex-col">
+              <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Gallery</li>
+                <li>Contact Us</li>
+              </ul>
             </div>
-
-            <button className="btn btn-primary">SUBSCRIBE</button>
+            <div className="flex-col">
+              <ul>
+                <li>Contents</li>
+                <li>Stories</li>
+                <li>Guides</li>
+              </ul>
+            </div>
           </div>
-        </form>
-        <span>
-          Want to become a contributor? <a href="/contribute">Click here</a>
-        </span>
+          <div className="flex-row">
+            <button className="btn btn-secondary">
+              SUPPORT US <ArrowRight />
+            </button>
+            <p>
+              fund independent journal
+              <br /> with 10$ per month
+            </p>
+          </div>
+        </div>
+        <div className="footer-content-3">
+          <h3>
+            <Mail />
+            Subscribe Us
+          </h3>
+          <p>
+            Stay in the loop! Subscribe to get the latest updates, news, and
+            tips delivered straight to your inbox.
+          </p>
+          <form onSubmit={handleSubscribeUs}>
+            <div className="flex-col">
+              <div className="input-container">
+                <input
+                  name="user_name"
+                  type="text"
+                  placeholder="Your Email Address"
+                />
+              </div>
+
+              <button className="btn btn-primary">SUBSCRIBE</button>
+            </div>
+          </form>
+          <span>
+            Want to become a contributor? <a href="/contribute">Click here</a>
+          </span>
+        </div>
       </div>
     </footer>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,12 +8,12 @@ import { useState } from "react";
 function HomePage() {
   const [ordering, setOrdering] = useState("-created_at");
   return (
-    <div>
+    <>
       <Navbar />
       <Filter ordering={ordering} setOrdering={setOrdering} />
       <BlogList ordering={ordering} />
       <Footer />
-    </div>
+    </>
   );
 }
 
