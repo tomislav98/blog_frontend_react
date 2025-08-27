425e4f19731c468bfaa309005dd2ee315ec46c33
kaido98 <kaido98@fedora.fritz.box>
2025-08-27 21:56:51 +0200

    Add Projects section heading and update layout styles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,35 +6,38 @@ function Projects() {
   return (
     <section className="projects">
       <div className="container">
-        <div className="flex-row">
-          <div className="card">
-            <div className="header-section">
-              <BriefcaseBusiness className="icon" />
+        <div className="flex-col">
+          <h2 className="text-xxl">Projects</h2>
+          <div className="flex-row">
+            <div className="card">
+              <div className="header-section">
+                <BriefcaseBusiness className="icon" />
+              </div>
+              <div className="content-section">
+                <h3>Completed</h3>
+                <p>15+ Finished projects</p>
+              </div>
             </div>
-            <div className="content-section">
-              <h3>Completed</h3>
-              <p>15+ Finished projects</p>
-            </div>
-          </div>
-          <div className="card">
-            <div className="header-section">
-              <Handshake className="icon" />
+            <div className="card">
+              <div className="header-section">
+                <Handshake className="icon" />
+              </div>
+
+              <div className="content-section">
+                <h3>Clients</h3>
+                <p>25+ Happy Clients</p>
+              </div>
             </div>
 
-            <div className="content-section">
-              <h3>Clients</h3>
-              <p>25+ Happy Clients</p>
-            </div>
-          </div>
+            <div className="card">
+              <div className="header-section">
+                <Star className="icon" />
+              </div>
 
-          <div className="card">
-            <div className="header-section">
-              <Star className="icon" />
-            </div>
-
-            <div className="content-section">
-              <h3>Experience</h3>
-              <p>7+ Years in the field</p>
+              <div className="content-section">
+                <h3>Experience</h3>
+                <p>7+ Years in the field</p>
+              </div>
             </div>
           </div>
         </div>
